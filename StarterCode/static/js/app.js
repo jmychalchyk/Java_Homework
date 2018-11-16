f96c4488395755a732fa157f1169d65a4bad7541
Jeffrey Mychalchyk <40670518+jmychalchyk@users.noreply.github.com>
2018-11-15 22:00:17 -0500

    update

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,28 +2,31 @@
 var tableData = data;
 var tbody = d3.select("tbody");
 
-function buildTbl(table) {tableData.forEach((siting) => {
-    // tbody.html("");
-    // d3.select("span").html("");
+function buildTbl(table) 
+{
+  table.forEach((sighting) => 
+  {
     var row = tbody.append("tr");
-  Object.entries(siting).forEach(([key, value]) => {
-    var cell = tbody.append("td");
-    cell.text(value);
+    Object.entries(sighting).forEach(([key, value]) => 
+    {
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
   });
-});
 }
 
 buildTbl(tableData);
 
-var btnFilter = d3.select("#filter-btn");
 
-btnFilter.on("click", function() {
+function clickFilter() 
+{
     d3.event.preventDefault();
-    var inputElement = d3.select("#datetime").property("value");
-    console.log(inputElement)
-    var filteredData = tableData.filter(rw => rw.datetime === inputElement);
-    console.log(filteredData)
     tbody.html("");
-    d3.select("span").html("");
+    var inputElement = d3.select("#datetime").property("value");
+    var filteredData = tableData.filter(rw => rw.datetime === inputElement);
+
     buildTbl(filteredData);
-    });  
+};  
+
+d3.select("#filter-btn").on('click', clickFilter);
+ 
